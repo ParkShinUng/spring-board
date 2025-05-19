24a6a3214c4e81fd643ae4f0de1525fd3b040bf6
psw <tlsdnd001@naver.com>
2025-05-19 22:23:53 +0900

    Spring-Question-Board Source First Commit

---
+++ src/main/java/spring/spring_question_board/SpringQuestionBoardApplication.java
@@ -0,0 +1,13 @@
+package spring.spring_question_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuestionBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuestionBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/spring_question_board/SpringQuestionBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.spring_question_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuestionBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
