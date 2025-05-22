38255535fabbffbe314323167a3766bc53b981f9
psw <tlsdnd001@naver.com>
2025-05-22 19:54:46 +0900

    로그인, 로그아웃 기능 구현

--- src/main/java/spring/spring_question_board/user/UserRepository.java
+++ src/main/java/spring/spring_question_board/user/UserRepository.java
@@ -2,5 +2,8 @@ package spring.spring_question_board.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
--- src/main/java/spring/spring_question_board/user/UserService.java
+++ src/main/java/spring/spring_question_board/user/UserService.java
@@ -1,7 +1,6 @@
 package spring.spring_question_board.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
