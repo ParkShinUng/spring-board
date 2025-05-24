660ee4569b4c895d30891cd6c4f743e2646ce50d
psw <tlsdnd001@naver.com>
2025-05-24 19:58:52 +0900

    답변 수정 완료 후 해당 답변으로 이동 기능 추가

--- src/main/java/spring/spring_question_board/answer/AnswerController.java
+++ src/main/java/spring/spring_question_board/answer/AnswerController.java
@@ -42,8 +42,8 @@ public class AnswerController {
             return "question_detail";
         }
 
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -72,7 +72,7 @@ public class AnswerController {
         }
 
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -93,7 +93,8 @@ public class AnswerController {
     public String answerVote(Principal principal, @PathVariable("id") Integer id) {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
+
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 }
