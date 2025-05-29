ba091e031355c046573a9110b1e180d059ef873c
psw <tlsdnd001@naver.com>
2025-05-29 20:46:14 +0900

    계정 관련 파일 분리

--- src/main/java/spring/spring_question_board/SpringQuestionBoardApplication.java
+++ src/main/java/spring/spring_question_board/SpringQuestionBoardApplication.java
@@ -2,7 +2,9 @@ package spring.spring_question_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:secrets.properties")
 @SpringBootApplication
 public class SpringQuestionBoardApplication {
 
